b4bce19a41765df3367a61a62d0d452615c39f13
Preet Khowaja <preet.khowaja@duke.edu>
2022-03-28 22:40:56 -0400

    created classes for patient, lab; updated parsing and age function

--- ehr_analysis.py
+++ ehr_analysis.py
@@ -2,95 +2,137 @@
 from datetime import datetime, timedelta, date
 from typing import TypeVar, Dict, List
 
+# Initiating Classes for Patient and Lab
+class Patient:
+    def __init__(self, id: str, gender: str, dob: str, race: str, labs: list) -> None:
+        self.id = id
+        self.gender = gender
+        self.dob = datetime.strptime(dob, "%Y-%m-%d %H:%M:%S.%f")
+        self.race = race
+        self.lab = labs
 
-def parse_data(filename: str) -> Dict[str, List[str]]:
-    """I have used a dictionary where each patient's data
-    can be accessed using PatientID so that the computational
-    complexity is lower than using a list. I can access records
-    using PatientID with O(1) complexity."""
-    """The complexity of this parser is better since we add the data 
-    to a dictionary as we read it. This parser likely has complexity
-    O(3N)"""
+    def __str__(self) -> str:
+        return "Patient: " + str(self.id)
+
+    @property
+    def age(self):
+        """The age property"""
+        time_diff = datetime.now() - self.dob
+        return time_diff.total_seconds() / 31536000
+
+    @property
+    def age_at_first_admit(self):
+        """Patient age at first admission"""
+        earliest_date = self.labs[0].lab_date
+        for L in self.labs[1:]:
+            if L.lab_date <= earliest_date:
+                earliest_date = L.lab_date
+        if self.labs == []:
+            raise ValueError("Patient not in records")
+        return earliest_date - self.dob
+
+
+class Lab:
+    def __init__(
+        self, p_id: str, name: str, value: str, units: str, lab_date: str
+    ) -> None:
+
+        self.p_id = p_id
+        self.name = name
+        self.value = float(value)
+        self.units = units
+        self.lab_date = lab_date
+
+    def __str__(self) -> str:
+        return "Lab " + self.name + " for Patient " + self.p_id
+
+
+def parse_data(
+    lab_file: str, patient_file: str, get_lab_dict: bool = False
+):  # -> Dict[str, List[str]]:
+
     patient_dict = {}  # 1
-    with open(filename) as f:  # 1
+    lab_dict = {}
+
+    # Populate lab dictionary with lab object
+    with open(lab_file) as f:  # 1
+        counter = 0
         for line in f:  # N for number of records
-            fields = line.split("\t")  # N times
-            patient_dict[fields[0]] = fields[1:]  # N times
-            # Check whether the dictionary contains the right
-            # number of fields for each key value
-            assert len(patient_dict[fields[0]]) == len(fields[1:])
-    return patient_dict
+            counter += 1
+            if counter == 1:
+                pass
+            else:
+                fields = line.split("\t")
+                lab_date = datetime.strptime(fields[5][0:23], "%Y-%m-%d %H:%M:%S.%f")
+                lab_object = Lab(fields[0], fields[2], fields[3], fields[4], lab_date)
+                if lab_object.p_id in lab_dict:
+                    # append to value of exisitng id
+                    lab_dict[lab_object.p_id].append(lab_object)
+                    pass
+                else:
+                    lab_dict[lab_object.p_id] = [lab_object]
+
+    # Populate Patient dict with Patient objects
+    with open(patient_file) as f:
+        counter = 0
+        for line in f:
+            counter += 1
+            if counter == 1:
+                pass
+            else:
+                patient_list_type = line.split("\t")
+                patient_ID = patient_list_type[0]
+                patient_obj = Patient(
+                    patient_ID,
+                    patient_list_type[1],
+                    patient_list_type[2],
+                    patient_list_type[3],
+                    lab_dict[patient_ID],
+                )
+                patient_dict[patient_ID] = patient_obj
+
+    # Return dictionaries
+    if get_lab_dict:
+        return patient_dict, lab_dict
+    else:
+        return patient_dict
+
+
+if __name__ == "__main__":
+    ## Here we import the objects into a dictionary
+    patient_file = "PatientCorePopulatedTable.txt"
+    lab_file = "LabsCorePopulatedTable.txt"
+    patient_dict, lab_dict = parse_data(lab_file, patient_file, get_lab_dict=True)
 
 
 # We access the above patient_dict to return number of
 # patients older than input age
 def num_older_than(age: int) -> int:
-    """returns number of patients older than given age"""
-    """The big O notationa for this is O(6N)"""
     count = 0  # 1
-    for key in patient_dict:  # Everything in this loop happens N times
-        patient = patient_dict[key]  # 1
-        age_str = patient[1]  # 1
-        if not age_str.isalpha():
-            time_diff = datetime.now() - datetime.strptime(
-                age_str, "%Y-%m-%d %H:%M:%S.%f"
-            )  # 1
-            years = time_diff.total_seconds() / 31536000  # 1
-            assert type(years) == float
-            if years > age:  # 1 in dictionaries
-                count += 1  # 1
-    # The total number of returns has to be less than the length of the dictionary
-    assert count <= len(patient_dict)
+    for P in patient_dict:
+        if P.age > age:
+            count += 1
     return count
 
 
-def sick_patients(lab: str, gt_lt: str, value: int) -> List[str]:
-    """returns patient ID with characteristics input for lab test"""
-    """Complexity is O(5N)"""
-    list_of_pid = []
-    # first go through dictionary and find patients with provided labs
-    # this has the advantage of returning only unique patient IDs
-    for key, vals in lab_dict.items():  # N
-        if vals[1] == lab:  # 1 in dictionaries
-            if gt_lt == "<":  # 1
-                if int(float(vals[2])) < value:  # 1
-                    list_of_pid.append(key)  # 1
-            elif gt_lt == ">":
-                if int(float(vals[2])) > value:
-                    list_of_pid.append(key)
-            else:
-                raise ValueError("Please enter < or > as the second argument.")
-    # Check if the length of the list is not more than number of observations
-    # in the data
-    assert len(list_of_pid) <= len(lab_dict)
-    return list_of_pid
-
-
-def patient_age(p_id: str) -> float:
-    """takes patient ID as input and returns patient's age at first admission"""
-    info = patient_dict[p_id]
-    time_diff = datetime.now() - datetime.strptime(info[1], "%Y-%m-%d %H:%M:%S.%f")
-    age = time_diff.total_seconds() / 31536000
-    assert type(age) == float
-    return age
-
-
-if __name__ == "__main__":
-    ## Here we import the data into a dictionary
-    patient_dict = parse_data("PatientCorePopulatedTable.txt")
-    lab_dict = parse_data("LabsCorePopulatedTable.txt")
-    print(patient_dict)
-    print(patient_age("0681FA35-A794-4684-97BD-00B88370DB41"))
-
-
-# Initiating Classes for Patient and Lab
-
-
-class Patient:
-    def __init__(self, id) -> None:
-        pass
-
-
-class Lab:
-    def __init__(self) -> None:
-        pass
+# NEED TO ADAPT TO CLASSES
+# def sick_patients(lab: str, gt_lt: str, value: int) -> list:
+#     """returns patient ID with characteristics input for lab test"""
+#     """Complexity is O(5N)"""
+#     list_of_pid = []
+#     # first go through dictionary and find patients with provided labs
+#     # this has the advantage of returning only unique patient IDs
+#     for key, vals in lab_dict.items():  # N
+#         if vals[1] == lab:  # 1 in dictionaries
+#             if gt_lt == "<":  # 1
+#                 if int(float(vals[2])) < value:  # 1
+#                     list_of_pid.append(key)  # 1
+#             elif gt_lt == ">":
+#                 if int(float(vals[2])) > value:
+#                     list_of_pid.append(key)
+#             else:
+#                 raise ValueError("Please enter < or > as the second argument.")
+#     # Check if the length of the list is not more than number of observations
+#     # in the data
+#     assert len(list_of_pid) <= len(lab_dict)
+#     return list_of_pid
