f36258e820bbef579d03f2cced15d1f118e243f6
Preet Khowaja <preet.khowaja@duke.edu>
2022-04-15 19:41:15 -0400

    added more tests

--- ehr_analysis.py
+++ ehr_analysis.py
@@ -1,6 +1,8 @@
 ### This module contains functions to parse and analyze patient data
 from datetime import datetime, timedelta, date
+from fileinput import filename
 from typing import TypeVar, Dict, List
+import pytest
 
 
 def parse_data(filename: str) -> Dict[str, List[str]]:
@@ -17,21 +19,21 @@ def parse_data(filename: str) -> Dict[str, List[str]]:
             fields = line.split("\t")  # N times
             # Strip the trailing newline character
             updated_last_word = fields[-1].strip()
-            patient_dict[fields[0]] = fields[1:-1] + [updated_last_word]  # N times
-            # Check whether the dictionary contains the right
-            # number of fields for each key value
-            assert len(patient_dict[fields[0]]) == len(fields[1:])
+            if fields[0] not in patient_dict:
+                patient_dict[fields[0]] = fields[1:-1] + [updated_last_word]  # N times
+            else:
+                patient_dict[fields[0]].append(fields[1:-1] + [updated_last_word])
     return patient_dict
 
 
 # We access the above patient_dict to return number of
 # patients older than input age
-def num_older_than(age: int) -> int:
+def num_older_than(p_dict: dict, age: int) -> int:
     """returns number of patients older than given age"""
     """The big O notationa for this is O(6N)"""
     count = 0  # 1
-    for key in patient_dict:  # Everything in this loop happens N times
-        patient = patient_dict[key]  # 1
+    for key in p_dict:  # Everything in this loop happens N times
+        patient = p_dict[key]  # 1
         age_str = patient[1]  # 1
         if not age_str.isalpha():
             time_diff = datetime.now() - datetime.strptime(
@@ -42,14 +44,14 @@ def num_older_than(age: int) -> int:
             if years > age:  # 1 in dictionaries
                 count += 1  # 1
     # The total number of returns has to be less than the length of the dictionary
-    assert count <= len(patient_dict)
     return count
 
 
-def sick_patients(lab: str, gt_lt: str, value: int) -> List[str]:
+def sick_patients(lab_dict: dict, lab: str, gt_lt: str, value: int) -> List[str]:
     """returns patient ID with characteristics input for lab test"""
     """Complexity is O(5N)"""
     list_of_pid = []
+
     # first go through dictionary and find patients with provided labs
     # this has the advantage of returning only unique patient IDs
     for key, vals in lab_dict.items():  # N
@@ -60,26 +62,13 @@ def sick_patients(lab: str, gt_lt: str, value: int) -> List[str]:
             elif gt_lt == ">":
                 if int(float(vals[2])) > value:
                     list_of_pid.append(key)
-            else:
-                raise ValueError("Please enter < or > as the second argument.")
-    # Check if the length of the list is not more than number of observations
-    # in the data
-    assert len(list_of_pid) <= len(lab_dict)
     return list_of_pid
 
 
-def patient_age(p_id: str) -> float:
+def patient_age(p_dict: dict, p_id: str) -> float:
     """takes patient ID as input and returns patient's age at first admission"""
-    info = patient_dict[p_id]
+    info = p_dict[p_id]
     time_diff = datetime.now() - datetime.strptime(info[1], "%Y-%m-%d %H:%M:%S.%f")
     age = time_diff.total_seconds() / 31536000
     assert type(age) == float
     return age
-
-
-if __name__ == "__main__":
-    ## Here we import the data into a dictionary
-    patient_dict = parse_data("PatientCorePopulatedTable.txt")
-    lab_dict = parse_data("LabsCorePopulatedTable.txt")
-    print(patient_dict)
-    print(patient_age("0681FA35-A794-4684-97BD-00B88370DB41"))
--- test.py
+++ test.py
@@ -1,23 +1,69 @@
 # This module tests all the functionalities in the ehr_analysis
+import pytest
 from datetime import datetime, timedelta, date
 from typing import TypeVar, Dict, List
-from ehr_analysis import parse_data, num_older_than, sick_patients, patient_age
+from ehr_analysis import (
+    parse_data,
+    num_older_than,
+    sick_patients,
+    patient_age,
+)
 
 
 def test():
     patient_dictionary_desired = {
-        "PatientID   ": [
-            "PatientName ",
-            "PatientGender   ",
-            "PatientDateofBirth ",
-            "PatientRace",
+        "PatientID": ["PatientGender", "PatientDateOfBirth", "PatientRace"],
+        "FB2ABB23-C9D0-4D09-8464-49BF0B982F0F": [
+            "Male",
+            "1947-12-28 02:45:40.547",
+            "Unknown",
         ],
-        "123": ["Preet   ", "Female ", " 1997-05-08 02:45:40.547 ", "Asian"],
-        "456": ["Liam Reese  ", "Male    ", "1969-11-28 11:39:49.197", "White"],
+        "64182B95-EB72-4E2B-BE77-8050B71498CE": [
+            "Male",
+            "1952-01-18 19:51:12.917",
+            "African American",
+        ],
+        "DB22A4D9-7E4D-485C-916A-9CD1386507FB": [
+            "Female",
+            "1970-07-25 13:04:20.717",
+            "Asian",
+        ],
+        "6E70D84D-C75F-477C-BC37-9177C3698C66": [
+            "Male",
+            "1999-07-25 13:04:20.718",
+            "White",
+        ],
     }
+    patient_dict = parse_data("sample_patient.txt")
+    assert patient_dict == patient_dictionary_desired
 
-    assert parse_data("patient_sample.txt") == patient_dictionary_desired
+    # Checking if num older than returns patients less than the number present in the dictionary
+    assert num_older_than(patient_dict, 0) < (len(patient_dict) + 1)
+    # Check if it returns all patients if asked for those with age older than 0
+    assert num_older_than(patient_dict, 0) == 4
+
+    # Check lab dictionary output
+    lab_dictionary = parse_data("sample_lab.txt")
+    desired_lab = {
+        "PatientID": ["AdmissionID", "LabName", "LabValue", "LabUnits", "LabDateTime"],
+        "1A8791E3-A61C-455A-8DEE-763EB90C9B2C": [
+            "1",
+            "URINALYSIS: RED BLOOD CELLS",
+            "1.8",
+            "rbc/hpf",
+            "1992-07-01 01:36:17.910",
+            ["1", "METABOLIC: GLUCOSE", "103.3", "mg/dL", "1992-06-30 09:35:52.383"],
+            ["1", "CBC: MCH", "35.8", "pg", "1992-06-30 03:50:11.777"],
+            ["1", "METABOLIC: CALCIUM", "8.9", "mg/dL", "2005-07-31 19:56:49.560"],
+        ],
+    }
+    assert lab_dictionary == desired_lab
+    # Check sick_patient function
+    assert sick_patients(lab_dictionary, "URINALYSIS: RED BLOOD CELLS", ">", 0) == [
+        "1A8791E3-A61C-455A-8DEE-763EB90C9B2C"
+    ]
 
 
 if __name__ == "__main__":
-    test()
+    # test()
+    pytest.main(["test.py"])
